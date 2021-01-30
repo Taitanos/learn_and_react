3c9617c307dc5866233a3ce9aad0bbace1fe7521
Taitanos <hayaashi87@gmail.com>
2021-01-30 16:51:03 +0300

    refactoring and add props

--- src/App.tsx
+++ src/App.tsx
@@ -1,71 +1,38 @@
 import React from 'react';
 import './App.css';
+import Accordion from "./components/Accordion/Accordion";
+import {Rating} from "./components/Rating/Rating";
+
 
 function App() {
     return (
         <div className="App">
-            <AppTitle/>
-            <Rating/>
-            <Accordion/>
-            <Rating/>
+            <PageTitle title={"This is APP component"}/>
+            <PageTitle title={"My books"}/>
+            Article 1
+            <Rating value={3} />
+            <Accordion titleValue={"Menu"} collapsed={false} />
+            <Accordion titleValue={"HarryPotter"} collapsed={true} />
+            Article 2
+            <Rating value={4} />
+            <Rating value={0} />
+            <Rating value={1} />
+            <Rating value={2} />
+            <Rating value={3} />
+            <Rating value={4} />
+            <Rating value={5} />
         </div>
     );
 }
 
-function AppTitle() {
-    console.log("AppTitle rendering")
-    return (
-        <>This is APP component</>
-    )
+type PageTitlePropsType = {
+    title: string
 }
 
-function Rating() {
-    console.log("Rating rendering")
+function PageTitle(props: PageTitlePropsType) {
+    console.log("PageTitle rendering")
     return (
-        <>
-            <Star/>
-            <Star/>
-            <Star/>
-            <Star/>
-            <Star/>
-        </>
-    )
-}
-
-function Accordion() {
-    console.log("Accordion rendering")
-    return (
-        <div>
-            <AccordionTitle/>
-            <AccordionBody/>
-        </div>
-    )
-}
-
-function AccordionTitle() {
-    console.log("AccordionTitle rendering")
-    return (
-        <h3>Menu</h3>
-    )
-}
-
-function AccordionBody() {
-    console.log("AccordionBody rendering")
-    return (
-        <ul>
-            <li>1</li>
-            <li>2</li>
-            <li>3</li>
-        </ul>
-    )
-}
-
-function Star() {
-    console.log("Star rendering")
-    return (
-        <div>
-            <div>Star</div>
-        </div>
+        <h1>{props.title}</h1>
     )
 }
 
